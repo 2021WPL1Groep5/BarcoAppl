643b081fae5e567e9671a2b5f4773ad828fe049b
robbe.delsoir <robbe.delsoir@student.vives.be>
2021-03-04 14:02:16 +0100

    constants

---
+++ BarcoApplicatie/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarcoApplicatie
+{
+    public class Constants
+    {
+        //change to personal connection
+        public static string CONNECTION_STRING =
+           "Server=DESKTOP-MB87VP9\\VIVES; Database= Barco2021; Trusted_Connection=True;";
+    }
+}
