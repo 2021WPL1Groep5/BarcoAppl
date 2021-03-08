51d80110afb0c1486305653b1cc807c6894263a1
MathiasVandoorneVives <mathias.vandoorne@student.vives.be>
2021-03-08 09:47:29 +0100

    error class aanmaken

---
+++ BarcoApplicatie/ErrorMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+
+namespace BarcoApplicatie
+{
+     class ErrorMessage
+    {
+        public string txtProjectNumber { get; set; }
+        public string txtNetWeight { get; set; }
+
+        public NummerProject(string txtprojectnr )
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtprojectnr, "[^0-9-E-.]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                txtprojectnr = txtprojectnr.Remove(txtprojectnr.Length - 1);
+            }
+        }
+
+        public netWeight(string txtnetweight)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtnetweight, "[^0-9-,]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                txtnetweight = txtnetweight.Remove(txtnetweight.Length - 1);
+            }
+        }
+
+    }
+}
