61257896652848c30b8fe3b25040c915f84655ca
koent <koent@LAPTOP>
2021-03-11 16:41:10 +0100

    "Master" backup

--- BarcoApplicatie/Constants.cs
+++ BarcoApplicatie/Constants.cs
@@ -8,7 +8,7 @@ namespace BarcoApplicatie
     {
         //change to personal connection
         public static string CONNECTION_STRING =
-           "Server=DESKTOP-20TBGJO\\SQLEXPRESS; Database= Barco 2021; Trusted_Connection=True;";
+           "Server=LAPTOP\\VIVES; Database= Barco; Trusted_Connection=True;";
     }
     
 }
