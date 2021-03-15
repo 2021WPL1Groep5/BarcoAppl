f991cd9b5de9cb8d2df47993b5d0c6f87a46ac4e
NikkiCobra <nikki.noppe@student.vives.be>
2021-03-15 11:58:55 +0100

    boolCheckBox

--- BarcoApplicatie/MainWindow.xaml.cs
+++ BarcoApplicatie/MainWindow.xaml.cs
@@ -125,7 +125,7 @@ namespace BarcoApplicatie
             dao.addingOptionalInput(txtLinkToTestplan.Text, txtSpecialRemarks.Text);
         }
 
-        ///////////////////////////////////////////toggleCheckbox///////////////////////////////////////////
+        ///////////////////////////////////////////boolCheckbox///////////////////////////////////////////
         public static class Utils
         {
             public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
@@ -148,7 +148,7 @@ namespace BarcoApplicatie
                 }
             }
         }
-        public void toggleCheckboxes(string checkboxName, string exceptions ,bool toggle)
+        public void boolCheckboxes(string checkboxName, string exceptions ,bool toggle)
         {
             foreach (CheckBox checkBox in Utils.FindVisualChildren<CheckBox>(this))
             {
@@ -156,6 +156,10 @@ namespace BarcoApplicatie
                     if (!exceptions.Contains(checkBox.Name))
                     {
                         checkBox.IsEnabled = toggle;
+                        if (checkBox.IsEnabled == false)
+                        {
+                            checkBox.IsChecked = false;
+                        }
                     }
                 }
             }
@@ -164,16 +168,17 @@ namespace BarcoApplicatie
         {
             if (name.IsChecked == true)
             {
-                toggleCheckboxes(cbname, name.Name, true);
+                boolCheckboxes(cbname, name.Name, true);
             }
             else
             {
-                toggleCheckboxes(cbname, name.Name, false);
+                boolCheckboxes(cbname, name.Name, false);
             }
         }
+        
+        //Nature of tests to perform
         private void cbEmcEut_Click(object sender, RoutedEventArgs e)
         {
-            //dateEmpty();
             toggle_click(cbEmcEut, "cbEmcEut");
         }
         private void cbEnviromental_Click(object sender, RoutedEventArgs e)
@@ -196,16 +201,170 @@ namespace BarcoApplicatie
         {
             toggle_click(cbGreenCompilance, "cbGreenCompilance");
         }
-
+        //Check if checkbox is empty for datePicker
         private void dateEmpty(DatePicker DateEut, CheckBox cbEmcEut, CheckBox cbEnviromental, CheckBox cbReliability, CheckBox cbProductSafety, CheckBox cbPackaging, CheckBox cbGreenCompilance)
         {
             if (cbEmcEut.IsChecked == false && cbEnviromental.IsChecked == false && cbReliability.IsChecked == false && cbProductSafety.IsChecked == false && cbPackaging.IsChecked == false && cbGreenCompilance.IsChecked == false)
             {
                 DateEut.IsEnabled = false;
             }
+            else
+            {
+                DateEut.IsEnabled = true;
+            }
         }
-        ///////////////////////////////////////////errorHandling///////////////////////////////////////////
+        //EUT1 checkbox
+        private void cbEmcEut1_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut1, cbEmcEut1, cbEnviromental1, cbReliability1, cbProductSafety1, cbPackaging1, cbGreenCompilance1);
+        }
+        private void cbEnviromental1_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut1, cbEmcEut1, cbEnviromental1, cbReliability1, cbProductSafety1, cbPackaging1, cbGreenCompilance1);
+        }
+        private void cbReliability1_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut1, cbEmcEut1, cbEnviromental1, cbReliability1, cbProductSafety1, cbPackaging1, cbGreenCompilance1);
+        }
+        private void cbProductSafety1_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut1, cbEmcEut1, cbEnviromental1, cbReliability1, cbProductSafety1, cbPackaging1, cbGreenCompilance1);
+        }
+        private void cbPackaging1_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut1, cbEmcEut1, cbEnviromental1, cbReliability1, cbProductSafety1, cbPackaging1, cbGreenCompilance1);
+        }
+        private void cbGreenCompilance1_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut1, cbEmcEut1, cbEnviromental1, cbReliability1, cbProductSafety1, cbPackaging1, cbGreenCompilance1);
+        }
+        //EUT2 checkbox
+        private void cbEmcEut2_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut2, cbEmcEut2, cbEnviromental2, cbReliability2, cbProductSafety2, cbPackaging2, cbGreenCompilance2);
+        }
+        private void cbEnviromental2_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut2, cbEmcEut2, cbEnviromental2, cbReliability2, cbProductSafety2, cbPackaging2, cbGreenCompilance2);
+        }
+        private void cbReliability2_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut2, cbEmcEut2, cbEnviromental2, cbReliability2, cbProductSafety2, cbPackaging2, cbGreenCompilance2);
+        }
+        private void cbProductSafety2_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut2, cbEmcEut2, cbEnviromental2, cbReliability2, cbProductSafety2, cbPackaging2, cbGreenCompilance2);
+        }
+        private void cbPackaging2_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut2, cbEmcEut2, cbEnviromental2, cbReliability2, cbProductSafety2, cbPackaging2, cbGreenCompilance2);
+        }
+        private void cbGreenCompilance2_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut2, cbEmcEut2, cbEnviromental2, cbReliability2, cbProductSafety2, cbPackaging2, cbGreenCompilance2);
+        }
+        //EUT3 checkbox
+        private void cbEmcEut3_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut3, cbEmcEut3, cbEnviromental3, cbReliability3, cbProductSafety3, cbPackaging3, cbGreenCompilance3);
+        }
+        private void cbEnviromental3_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut3, cbEmcEut3, cbEnviromental3, cbReliability3, cbProductSafety3, cbPackaging3, cbGreenCompilance3);
+        }
+        private void cbReliability3_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut3, cbEmcEut3, cbEnviromental3, cbReliability3, cbProductSafety3, cbPackaging3, cbGreenCompilance3);
+        }
+        private void cbProductSafety3_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut3, cbEmcEut3, cbEnviromental3, cbReliability3, cbProductSafety3, cbPackaging3, cbGreenCompilance3);
+        }
+        private void cbPackaging3_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut3, cbEmcEut3, cbEnviromental3, cbReliability3, cbProductSafety3, cbPackaging3, cbGreenCompilance3);
+        }
+        private void cbGreenCompilance3_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut3, cbEmcEut3, cbEnviromental3, cbReliability3, cbProductSafety3, cbPackaging3, cbGreenCompilance3);
+        }
+        //EUT4 checkbox
+        private void cbEmcEut4_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut4, cbEmcEut4, cbEnviromental4, cbReliability4, cbProductSafety4, cbPackaging4, cbGreenCompilance4);
+        }
+        private void cbEnviromental4_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut4, cbEmcEut4, cbEnviromental4, cbReliability4, cbProductSafety4, cbPackaging4, cbGreenCompilance4);
+        }
+        private void cbReliability4_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut4, cbEmcEut4, cbEnviromental4, cbReliability4, cbProductSafety4, cbPackaging4, cbGreenCompilance4);
+        }
+        private void cbProductSafety4_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut4, cbEmcEut4, cbEnviromental4, cbReliability4, cbProductSafety4, cbPackaging4, cbGreenCompilance4);
+        }
+        private void cbPackaging4_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut4, cbEmcEut4, cbEnviromental4, cbReliability4, cbProductSafety4, cbPackaging4, cbGreenCompilance4);
+        }
+        private void cbGreenCompilance4_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut4, cbEmcEut4, cbEnviromental4, cbReliability4, cbProductSafety4, cbPackaging4, cbGreenCompilance4);
+        }
+        //EUT5 checkbox
+        private void cbEmcEut5_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut5, cbEmcEut5, cbEnviromental5, cbReliability5, cbProductSafety5, cbPackaging5, cbGreenCompilance5);
+        }
+        private void cbEnviromental5_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut5, cbEmcEut5, cbEnviromental5, cbReliability5, cbProductSafety5, cbPackaging5, cbGreenCompilance5);
+        }
+        private void cbReliability5_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut5, cbEmcEut5, cbEnviromental5, cbReliability5, cbProductSafety5, cbPackaging5, cbGreenCompilance5);
+        }
+        private void cbProductSafety5_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut5, cbEmcEut5, cbEnviromental5, cbReliability5, cbProductSafety5, cbPackaging5, cbGreenCompilance5);
+        }
+        private void cbPackaging5_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut5, cbEmcEut5, cbEnviromental5, cbReliability5, cbProductSafety5, cbPackaging5, cbGreenCompilance5);
+        }
+        private void cbGreenCompilance5_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut5, cbEmcEut5, cbEnviromental5, cbReliability5, cbProductSafety5, cbPackaging5, cbGreenCompilance5);
+        }
+        //EUT6 checkbox
+        private void cbEmcEut6_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut6, cbEmcEut6, cbEnviromental6, cbReliability6, cbProductSafety6, cbPackaging6, cbGreenCompilance6);
+        }
+        private void cbEnviromental6_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut6, cbEmcEut6, cbEnviromental6, cbReliability6, cbProductSafety6, cbPackaging6, cbGreenCompilance6);
+        }
+        private void cbReliability6_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut6, cbEmcEut6, cbEnviromental6, cbReliability6, cbProductSafety6, cbPackaging6, cbGreenCompilance6);
+        }
+        private void cbProductSafety6_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut6, cbEmcEut6, cbEnviromental6, cbReliability6, cbProductSafety6, cbPackaging6, cbGreenCompilance6);
+        }
+        private void cbPackaging6_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut6, cbEmcEut6, cbEnviromental6, cbReliability6, cbProductSafety6, cbPackaging6, cbGreenCompilance6);
+        }
+        private void cbGreenCompilance6_Click(object sender, RoutedEventArgs e)
+        {
+            dateEmpty(DateEut6, cbEmcEut6, cbEnviromental6, cbReliability6, cbProductSafety6, cbPackaging6, cbGreenCompilance6);
+        }
 
+        ///////////////////////////////////////////errorHandling///////////////////////////////////////////
         public void ControlInput(string canBe, TextBox box, Label label, string content)
         {
             if (System.Text.RegularExpressions.Regex.IsMatch(box.Text, canBe))
