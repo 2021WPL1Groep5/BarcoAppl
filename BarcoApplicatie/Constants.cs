6da50ab5c6469abe5114471f3d908c219b896ce4
Mohamed2201 <mohamed.koraiche@student.vives.be>
2021-03-15 13:55:29 +0100

    constants Mohamed

--- BarcoApplicatie/Constants.cs
+++ BarcoApplicatie/Constants.cs
@@ -7,6 +7,6 @@ namespace BarcoApplicatie
     public class Constants
     {
         public static string CONNECTION_STRING =
-           "Server=DESKTOP-20TBGJO\\SQLEXPRESS; Database= Barco 2021; Trusted_Connection=True;";
+           "Server=MOKO\\VIVES; Database= Barco; Trusted_Connection=True;";
     }
 }
